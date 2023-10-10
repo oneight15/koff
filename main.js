5be7b7c7609f848a69f769305c901936d962953e
oneight15 <oneight1508@gmail.com>
2023-10-10 17:35:20 +0500

    feat: add render header, main and footer

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import 'normalize.css';
 import './style.scss';
 import Navigo from 'navigo';
+import { Header } from './modules/Header/Header';
+import { Main } from './modules/Main/Main';
+import { Footer } from './modules/Footer/Footer';
 
 
 
@@ -32,6 +35,10 @@ const productSlider = () => {
 };
 
 const init = () => {
+  new Header().mount();
+  new Main().mount();
+  new Footer().mount();
+
   productSlider();
 
   const router = new Navigo('/', {linksSelector: 'a[href^="/"]'});
